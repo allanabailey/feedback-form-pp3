b73936a01a69ca12b70dba93cb6440c8510c61d2
Allana Bailey <hmabailey@hotmail.com>
2022-02-03 21:34:58 +0000

    Convert the user input for month into an int value and convert it to month name using Calendar

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import calendar
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,9 +19,13 @@ def get_averages():
     based on the month selected by the user.
     """
     print("Please enter the month that you would like to view the feedback for...\n")
-    print("Note that months are numbered, e.g 1 = January, 2 = February... 11 == November\n")
+    print("Note that months are numbered, e.g 1 = January, 2 = February... 11 = November\n")
 
-    month_chosen_num = input("Enter your choice of month here:\n")
+    month_chosen_str = input("Enter your choice of month here:\n")
+    print(f"Month chosen: {month_chosen_str}")
+    month_chosen_num = int(month_chosen_str)
     print(f"Month chosen: {month_chosen_num}")
+    month_chosen_name = calendar.month_name[month_chosen_num]
+    print(month_chosen_name)
 
 get_averages()
