4005661423a08f99f03a3408e19bb95ea7b06876
Allana Bailey <hmabailey@hotmail.com>
2022-02-05 13:09:59 +0000

    Remove test code and return the new dictionary containing scores for each category

--- run.py
+++ run.py
@@ -63,20 +63,20 @@ def get_scores(month):
     scores = []
     scores_with_headers = {}
     headers = month_worksheet.row_values(1)[2:10]
+
     for num, header in zip(range(3,11), headers):
         score = month_worksheet.col_values(num)[1:]
         scores.append(score)
         scores_with_headers[header] = score
         print(f"{header}:  {score}")
 
-
     # print(f"Month data: {month_data}")
     print(f"Headers: {headers}")
     print(f"Scores: {scores}")
-    print(f"Scores with headers: {scores_with_headers}")
+
+    return scores_with_headers
 
 month = get_month()
-print(type(month))
-test_data = get_scores(month)
-# print(test_data)
+score_data = get_scores(month)
+print(score_data)
 # month_chosen_name = calendar.month_name[month_chosen_num]
